337a8c420054800bdfe9a35b14d22b0a7586b269
kaiquedevsp <kaiquejoquimsilva@gamil.com>
2021-09-29 19:10:03 -0300

    Função 'transactions' criada

--- scripts.js
+++ scripts.js
@@ -43,23 +43,39 @@ const transactions = [
     date: '28/09/2021',
   },
 ]
-// Eu preciso somar as entradas
-// sepois eu preciso somar as saídas e
-// remover das entradas o valor das saídas
-// assim, eu terei o total
 
 const Transaction = {
+  all: transactions,
+
+  add(transaction) {
+    Transaction.all.push(transaction)
+    App.reload()
+  },
+
   incomes() {
-    // somar as entradas
+    let income = 0;
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    })
+    return income;
   },
+
   expenses() {
-    // somar as saídas
+    let expense = 0;
+    Transaction.all.forEach(transaction => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    })
+    return expense;
   },
+
   total() {
-    // entradas - saídas
+    return Transaction.incomes() + Transaction.expenses();
   }
 }
-// Substituir os dados do HTML com os dados do JS
 
 const DOM = {
   transactionsContainer: document.querySelector('#data-table tbody'),
@@ -77,25 +93,62 @@ const DOM = {
 
     const html = `
       <td class="description">${transaction.description}</td>
-      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
         <img src="./assets/minus.svg" alt="Remover Transação">
       </td>
     `
     return html
+  },
+
+  updateBalance() {
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
   }
 }
 
 const Utils = {
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : ""
+    value = String(value).replace(/\D/g, "")
+    value = Number(value) / 100
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
 
-
-    console.log()
+    return signal + value
   }
 }
 
-transactions.forEach(function (transaction) {
-  DOM.addTransaction(transaction)
+const App = {
+  init() {
+
+    Transaction.all.forEach(transaction => {
+      DOM.addTransaction(transaction)
+    })
+
+    DOM.updateBalance()
+
+  },
+  reload() {
+    App.init()
+  },
+}
+
+App.init()
+
+Transaction.add({
+  id: 39,
+  description: 'Alo',
+  amount: 200,
+  date: '28/09/2021'
 })
